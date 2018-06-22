6a2f7b9e8732de562fa4ebf4c151fcf60dd0a27c
Tyler Thompson <tyler.l.thompson@wmich.edu>
2018-06-22 14:15:26 -0400

    fixed deepfreeze status

--- Classes/SshSendThreaded.py
+++ Classes/SshSendThreaded.py
@@ -22,10 +22,6 @@ class SshSendThreaded(threading.Thread):
         if self.tools.getPing(ip=self.computer.ip) == True:
             self.computer.functionReturn=(str(self.tools.sendSsh(user=self.computer.user, ip=self.computer.ip, cmd=self.computer.cmd))).replace("\n", "").replace("\t", "").replace(" ", "")
 
-            maxReturnLength = 60
-            if len(self.computer.functionReturn) > maxReturnLength:
-                self.computer.functionReturn = self.computer.functionReturn[:maxReturnLength - 3] + "..."
-
             # detect if reading a deep freeze status report
             if self.computer.dfstatus == True:
                 if "BOOTFROZEN" in self.computer.functionReturn:
@@ -34,6 +30,10 @@ class SshSendThreaded(threading.Thread):
                     self.computer.functionReturn = "THAWED"
                 else:
                     self.computer.functionReturn = "UNKNOWN"
+
+            maxReturnLength = 60
+            if len(self.computer.functionReturn) > maxReturnLength:
+                self.computer.functionReturn = self.computer.functionReturn[:maxReturnLength - 3] + "..."
 
             if "timed out" in self.computer.functionReturn:
                 self.computer.functionReturn = ("Connection timed out.")
